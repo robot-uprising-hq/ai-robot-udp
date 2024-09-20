13e18f8b063219b89c98b653ffa342e4f89c892f
Juho Kostet <juho@augnition.fi>
2024-09-20 19:56:41 +0300

    update test script to also stop robot:)

--- examples/send-udp.py
+++ examples/send-udp.py
@@ -1,4 +1,7 @@
 import socket
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes("50;-50", "utf-8"), ("192.168.1.105", 3000))
+sock.sendto(bytes("-100;-100", "utf-8"), ("192.168.1.114", 3000))
+time.sleep(1)
+sock.sendto(bytes("0;0", "utf-8"), ("192.168.1.114", 3000))
